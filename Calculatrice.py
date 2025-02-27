6da90deb69b53c1793ddb17f21471191fbc06866
Mamen_Kader <mamenkader@gmail.com>
2025-02-27 20:54:32 +0100

    Mon premier programme

---
+++ Calculatrice.py
@@ -0,0 +1,3 @@
+a = input("Veuillez entrer un premier nombre : ")
+b = input("Veuillez entrer un second nombre : ")
+print(f"Le résultat de l'addition du nombre {a} avec le nombre {b} est égale à {int(a) + int(b)}")
